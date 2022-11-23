9a644562f3d60ac5c7c8dacda0bd3e99126e9dbc
Rodrigo Farias <rodrigofarias08@gmail.com>
2022-11-22 22:33:05 -0300

    Retificado erros

--- forca.js
+++ forca.js
@@ -4,7 +4,7 @@
 //Em seguida, pedir letras até que todas as letras da palavra sejam mencionadas.
 
 
-let palavra = prompt("Digite uma palavra:").toUpperCase()
+let palavra = prompt("Digite uma palavra ou texto:").toUpperCase()
 
 let nLetras = palavra.length
 
@@ -20,35 +20,30 @@ palavra.split("").forEach(element => {
 // Enquanto alguma letra permanecer oculta (_) continua pedindo letras
 while (letrasDescobertas.includes("_")) {
 
-  let letra = prompt(`Letras descobertas:
+  let letra = prompt(`Caracteres descobertos:
     ${letrasDescobertas}
 
     Digite a próxima letra:`).toUpperCase().charAt(0)
 
-  for (let i = 1; i < nLetras*2; i= i+2) {
+  for (let i = 1; i < nLetras * 2; i = i + 2) {
 
-    alert(`      
-      letra: ${letra}
-      palavra.charAt(i): ${letrasOcultas.charAt(i)}`)
-    
     if (letra === letrasOcultas.charAt(i)) {
 
-
       //letrasDescobertas recebe a letra digitada:
-      let parte1 = letrasDescobertas.substring(0, i - 1) 
+      let parte1 = letrasDescobertas.substring(0, i)
 
-      
       let parte2 = letrasDescobertas.substring(i + 1)
       letrasDescobertas = parte1 + letra + parte2
 
-      
-      alert(`Parabéns! Você descobriu a letra ${letra}!
-      
-      Parte1: ${parte1}
-      Parte2: ${parte2}`)
+      alert(`Parabéns! Você descobriu o caractere ${letra}!`)
 
     }
   }
 }
 
-alert(`Parabéns! Você descobriu a palavra ${palavra}!`)
+alert(`Parabéns! Você descobriu a palavra (ou texto) ${palavra}!`)
+
+
+
+
+
